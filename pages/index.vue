bfeab80424a49a71f10bb745e468e2f6bed6d539
mingming0919 <madelacruz423@gmail.com>
2024-02-26 08:19:36 +0800

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -4,9 +4,6 @@
             <div class="columns-1 md:columns-2 lg:columns-3 xl:columns-4 px-4 pt-4 gap-2">
                 <Weather />
                 <Calculator />
-                <Calculator />
-                <Calculator />
-                <Calculator />
             </div>
         </NuxtLayout>
     </div>
